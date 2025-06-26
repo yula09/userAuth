085579b4fad90731d5854719c656bca99e81ed2c
yula09 <kinley02wangmo@gmail.com>
2025-06-26 10:58:25 +0600

    Opening Project

--- Config/db.js
+++ Config/db.js
@@ -6,8 +6,8 @@ const pgp = pgpInit();
 
 
 const db = pgp({
-    host: 'localhost',
-    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    port: 5432, // Default PostgreSQL port
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
